c4708179b2af6d8b085f7705470d4a28f7d4312a
hr-meheraj <hrmeheraj2021@gmail.com>
2022-08-30 08:51:40 +0600

    Change font

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
-
 function App() {
   return (
     <div className="">
